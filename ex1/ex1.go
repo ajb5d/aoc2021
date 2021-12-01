7872c97708ce07667c109311063cec230494130e
Andrew Barros <ajb5d@virginia.edu>
2021-12-01 06:38:51 -0500

    Day 1

---
+++ ex1/ex1.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var (
+	inputFile = flag.String("input", "", "Input file")
+)
+
+func IngestIntegerListFile(inputPath string) []int {
+	output := []int{}
+	fileHandle, err := os.Open(inputPath)
+
+	if err != nil {
+		fmt.Println(err)
+		return output
+	}
+
+	defer fileHandle.Close()
+
+	scanner := bufio.NewScanner(fileHandle)
+	for scanner.Scan() {
+		val, _ := strconv.Atoi(scanner.Text())
+		output = append(output, val)
+	}
+
+	return output
+}
+
+func main() {
+	flag.Parse()
+	input := IngestIntegerListFile(*inputFile)
+
+	last := -1
+	incCount := 0
+
+	for _, val := range input {
+		if last != -1 && val > last {
+			incCount++
+		}
+		last = val
+	}
+
+	fmt.Printf("Incremented Count: %d\n", incCount)
+
+	last = -1
+	incCount = 0
+	for index, _ := range input[2:] {
+		currentSum := input[index] + input[index+1] + input[index+2]
+		if last != -1 && currentSum > last {
+			incCount++
+		}
+		last = currentSum
+	}
+	fmt.Printf("Incremented Count: %d\n", incCount)
+
+}
---
+++ ex1/go.mod
@@ -0,0 +1,3 @@
+module github.com/ajb5d/aoc2021/ex1
+
+go 1.17
